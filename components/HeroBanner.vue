7e928e777009a7cf887b3ce3c08463bf4185ef28
ABusbaher <a.busbaher@vegait.rs>
2022-10-05 11:45:45 +0200

    Add style for HeroBanner (without rectanges)

--- components/HeroBanner.vue
+++ components/HeroBanner.vue
@@ -4,34 +4,31 @@ export default {
   props: {
     imagePath: {
       type: String,
-      request: true
+      request: true,
+      default: ''
     },
     title: {
       type: String,
-      request: true
+      request: true,
+      default: ''
     },
     description: {
       type: String,
-      request: false
+      request: false,
+      default: ''
     }
   }
 }
 </script>
 <template>
-  <div class="banner-container">
-    <div class="hero-banner">
-<!--      <img :src="imagePath" :alt="title" class="hero-banner__img">-->
-<!--      <img :src="require('@/assets/images/V_redraw-without-bg-1_9.png')" alt="background img" class="hero-banner__bkg-img">-->
-      <div class="hero-banner__img"></div>
-      <div class="hero-banner__bkg-img"></div>
-      <h1 class="hero-banner__title">
-        {{ title }}
-      </h1>
-      <p class="hero-banner__desc">
-        {{ description }}
-      </p>
-      <div class="hero-banner__rectangle-4"></div>
-    </div>
-    <div class="rectangle-6" ></div>
+  <div class="hero-banner">
+    <img :src="imagePath" :alt="title" class="hero-banner__img">
+    <img :src="require('@/assets/images/V_redraw-without-bg-1_9.png')" alt="background img" class="hero-banner__bkg-img">
+    <h1 class="hero-banner__title">
+      {{ title }}
+    </h1>
+    <p class="hero-banner__desc">
+      {{ description }}
+    </p>
   </div>
 </template>
--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,10 +1,10 @@
 <script>
-import LogoNameSVG from "@/components/LogoNameSVG.vue"
+import LogoNameSVG from '@/components/LogoNameSVG.vue'
 
 export default {
   name: 'TheHeader',
   components: {
-    LogoNameSVG,
+    LogoNameSVG
   },
   data () {
     return {
@@ -33,19 +33,18 @@ export default {
     <div class="nav__menu">
       <NuxtLink class="nav__menu__logo" to="/">
         <img class="nav__menu__logo__img" src="@/assets/images/Vector.png" alt="logo">
-        <LogoNameSVG/>
+        <LogoNameSVG />
       </NuxtLink>
       <div class="nav__menu__icon" @click="showLinks = !showLinks">
         <img class="nav__menu__icon__img" src="@/assets/images/menu.png" alt="menu">
       </div>
     </div>
-      <ul class="nav__menu-links" v-show="showLinks">
-        <li class="nav__menu-links__link" v-for="link in links" :key="link.href">
-          <nuxt-link :to="link.href">
-            {{ link.title }}
-          </nuxt-link>
-        </li>
-      </ul>
-
+    <ul v-show="showLinks" class="nav__menu-links">
+      <li v-for="link in links" :key="link.href" class="nav__menu-links__link">
+        <nuxt-link :to="link.href">
+          {{ link.title }}
+        </nuxt-link>
+      </li>
+    </ul>
   </div>
 </template>
